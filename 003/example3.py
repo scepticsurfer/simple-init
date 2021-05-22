26ae1d61b251400a95fd2f65c213019795d2697e
scepticsurfer <79932624+scepticsurfer@users.noreply.github.com>
2021-05-22 16:47:18 +0300

    Add files via upload

---
+++ 003/example3.py
@@ -0,0 +1,10 @@
+# example3.py
+# this file uses __init__.py from string_func folder
+
+import string_func
+
+some_string = "Hello, Universe!"
+
+print(string_func.stringLength(some_string))
+print(string_func.stringToLower(some_string))
+print(string_func.stringToUpper(some_string))
